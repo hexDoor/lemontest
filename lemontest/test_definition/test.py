5dea3cca88554e0e8060208241d19403d72aaa1a
Kyu-Sang Kim <kyusangkim99@gmail.com>
2022-11-02 17:57:52 +1100

    upload to laptop

--- lemontest/test_definition/test.py
+++ lemontest/test_definition/test.py
@@ -176,7 +176,7 @@ class Test(AbstractTest):
         # check stdout stream
         stdout_short_explanation = self.check_stream(
             self.stdout, self.expected_stdout, "stdout"
-        
+        )
         # only consider stderr if unexpected stderr permitted or stdout issue discovered
         if not self.parameters["allow_unexpected_stderr"] or stdout_short_explanation:
             # early exit if we have a DCC issue + dcc early exit enabled
@@ -365,6 +365,64 @@ class Test(AbstractTest):
             output.close()
 
         return True
+    
+    def check_stream(self, actual, expected, name):
+        if self.debug:
+            print("name:", name)
+            print("actual:", actual[0:256] if actual else "")
+            print("expected:", expected[0:256] if expected else "")
+        if actual:
+            if expected:
+                # Handling non-unicode IO
+                if type(actual) in (bytearray, bytes) or type(expected) in (
+                    bytearray,
+                    bytes,
+                ):
+                    if actual == bytearray(expected):
+                        return None
+                    else:
+                        return "Your non-unicode output is not correct"
+                # handling unicode input
+                if compare_strings(actual, expected):
+                    return None
+                else:
+                    return "Incorrect " + name
+            else:
+                if name == "stderr":
+                    return "errors"
+                elif name == "stdout":
+                    return name + " produced when none expected"
+                else:
+                    return name + " should be empty and was not"
+        else:
+            if expected:
+                if name.lower().startswith("file"):
+                    return f"File {name} is empty"
+                else:
+                    return f"No {name} produced"
+            else:
+                return None
+
+    def check_files(self):
+        for (pathname, expected_contents) in self.parameters["expected_files"].items():
+            try:
+                if self.parameters["unicode_files"]:
+                    with open(pathname, encoding="UTF-8", errors="replace") as f:
+                        actual_contents = f.read()
+                else:
+                    with open(pathname, mode="rb") as f:
+                        actual_contents = f.read()
+            except IOError:
+                self.long_explanation = f"Your program was expected to create a file named '{pathname}' and did not\n"
+                actual_contents = ""
+            short_explanation = self.check_stream(
+                actual_contents, expected_contents, f"file: {pathname}"
+            )
+            if short_explanation:
+                self.file_not_ok = pathname
+                self.file_expected = expected_contents
+                self.file_actual = actual_contents
+                return short_explanation
 
     def params(self):
         return self.parameters
